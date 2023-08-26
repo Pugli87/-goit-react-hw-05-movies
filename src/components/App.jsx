995805cb41f17b9011dc5d3ca1dab773a148a308
Pugli87 <119467014+Pugli87@users.noreply.github.com>
2023-08-26 00:36:21 -0500

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,20 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Header } from './header/Header';
+import { Home } from 'pages/home/Home';
+import { Movies } from 'pages/movies/Movies';
+import { Error404 } from 'pages/error404/Error404';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="*" element={<Error404 />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 };
