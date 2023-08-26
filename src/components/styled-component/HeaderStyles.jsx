adc3df8c7f1162a4468fa62f7b895210bf15a241
Pugli87 <119467014+Pugli87@users.noreply.github.com>
2023-08-26 00:37:02 -0500

    Create HeaderStyles.jsx

---
+++ src/components/styled-component/HeaderStyles.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const HeaderStyles = styled.header`
+  margin: 0 auto;
+  display: flex;
+  align-items: center;
+  padding: 20px;
+  width: 100%;
+  box-shadow: 0 0 3px black;
+`;
