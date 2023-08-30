ba9ecb8db1150de0f4e72662e660d2e5e6600b46
Pugli87 <119467014+Pugli87@users.noreply.github.com>
2023-08-30 03:30:42 -0500

    Update moviesApi.js

--- src/components/moviesApi/moviesApi.js
+++ src/components/moviesApi/moviesApi.js
@@ -4,14 +4,12 @@ import Notiflix from "notiflix";
 
 const API_KEY = 'c0ec3039a6ea9335ddc382eb1bd446f2';
 const BASE_URL = 'https://api.themoviedb.org/3';
+export const baseImgURL = "https://image.tmdb.org/t/p/w300/"
+export const defaultImg = 'https://via.placeholder.com/300x450.png?text=Not+Found+Image';
 
 export const fetchPopularMovies = async () => {
   try {
-    const response = await axios.get(`${BASE_URL}/movie/popular`, {
-      params: {
-        api_key: API_KEY,
-      },
-    });
+    const response = await axios.get(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
     return response.data.results;
   } catch (error) {
     console.error(error);
