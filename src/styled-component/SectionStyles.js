3506f1b7ce28e48ca7b1b3fb3bdd9df8388f013d
Pugli87 <119467014+Pugli87@users.noreply.github.com>
2023-09-04 11:18:57 -0500

    Update SectionStyles.js max-width

--- src/styled-component/SectionStyles.js
+++ src/styled-component/SectionStyles.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const Section = styled.section`
   text-align: center;
-  width: 1280px;
+  max-width: 1280px;
   margin: 0 auto;
   padding: 16px;
   margin-top: 20px;
