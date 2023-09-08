0ae118e2b87b807729ba1d631d3daf1df47df924
Pugli87 <119467014+Pugli87@users.noreply.github.com>
2023-09-08 00:17:49 -0500

    logica para peticion por id

--- src/pages/movieDetails/MovieDetails.jsx
+++ src/pages/movieDetails/MovieDetails.jsx
@@ -1,49 +1,20 @@
-// import React, { useEffect, useState } from 'react';
-// import { useParams } from 'react-router-dom';
-// import { fetchMovieById } from 'components/moviesApi/moviesApi';
-
-// export function MovieDetails() {
-//   const { id } = useParams();
-//   const [movie, setMovie] = useState(null);
-
-//   useEffect(() => {
-//     fetchMovieById(id)
-//       .then(data => {
-//         setMovie(data);
-//         console.log(data);
-//       })
-//       .catch(error => {
-//         console.error(error);
-//       });
-//   }, [id]);
-
-//   if (!movie) {
-//     return <div>Loading...</div>;
-//   }
-
-//   return (
-//     <div>
-//       <h1>{movie.title}</h1>
-//       <p>{movie.overview}</p>
-//     </div>
-//   );
-// }
-
 import React, { useEffect, useState } from 'react';
-import { useParams, Outlet, Link } from 'react-router-dom';
-import { fetchPopularMovies } from 'components/moviesApi/moviesApi';
+import { useParams, /*Outlet,*/ Link } from 'react-router-dom';
+import {
+  baseImgURL,
+  defaultImg,
+  movieById,
+} from 'components/moviesApi/moviesApi';
 import { Section } from 'styled-component/SectionStyles';
 
 export default function MovieDetails() {
   const { movieId } = useParams();
-  console.log(movieId);
   const [movie, setMovie] = useState(null);
 
   const getMovies = () => {
-    fetchPopularMovies(movieId)
+    movieById(movieId)
       .then(results => {
         setMovie(results);
-        console.log(results);
       })
       .catch(error => {
         console.error(error);
@@ -60,13 +31,20 @@ export default function MovieDetails() {
 
   return (
     <Section>
-      <div className="flex items-center">
+      <div>
         <button type="button">
-          <i className="ri-arrow-left-line"></i>
           <Link to="/">Go Back</Link>
         </button>
       </div>
       <div>
+        <img
+          src={
+            movie?.backdrop_path
+              ? baseImgURL + movie?.backdrop_path
+              : defaultImg
+          }
+          alt={movie?.title || movie?.name}
+        />
         <h1>{movie.title}</h1>
         <p>{movie.overview}</p>
       </div>
