d86057aa0dde2f25aba02df9e2f7c164a73bf759
Pugli87 <119467014+Pugli87@users.noreply.github.com>
2023-09-08 00:15:39 -0500

    estilo active en pages
     se agrega basename a browserRouter

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import{ StrictMode } from 'react';
+//import{ StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter} from 'react-router-dom';
 import { App } from 'components/App';
@@ -8,9 +8,7 @@ const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
-  <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/goit-react-hw-05-movies">
       <App />
     </BrowserRouter>
-  </StrictMode>
 );
