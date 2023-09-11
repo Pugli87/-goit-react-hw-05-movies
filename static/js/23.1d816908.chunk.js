3b859b14da83577e263c5f617dc5d10600aac6da
Pugli87 <119467014+Pugli87@users.noreply.github.com>
2023-09-11 08:51:28 +0000

    Deploying to gh-pages from @ Pugli87/goit-react-hw-05-movies@ebb9ba0b6e5403b672fa412f4c6e1a6b320395fa 🚀

---
+++ static/js/23.1d816908.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[23],{23:function(e,t,i){i.r(t),i.d(t,{default:function(){return r}});i(791);var n=i(184);function r(){return(0,n.jsx)("div",{children:"Reviews"})}}}]);
+//# sourceMappingURL=23.1d816908.chunk.js.map
